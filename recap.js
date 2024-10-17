fd33a24ca03b2ac67569b286dc3d128a4754232e
sabbir1257 <masabbir1257@gmail.com>
2024-10-17 20:23:59 +0600

    get value push

--- recap.js
+++ recap.js
@@ -5,4 +5,11 @@
    2-2: getElementByClassName 
    2-3: querySekector
    2-4: querySelectorAll
+
+3. dynamically add element to the dom: document.createElement
+   3-1: set value to the innerHTML
+
+4. get value from a dom element 
+  4-1: use innerText (h1, p, h5, div, span, strong)
+  4-2: use value (input , textAreaf)
 */ 
