528d760793f38ffda3b0e4a6e44f62159d061fdc
sabbir1257 <masabbir1257@gmail.com>
2024-10-17 23:00:00 +0600

    validation file push

---
+++ validation.js
@@ -0,0 +1,14 @@
+const number = 23;
+// string, number, boolean
+if(typeof number === 'number'){
+     console.log('value is a number');
+}
+else{
+     console.log('value is not a number');
+}
+
+const numbers = [3, 12, 78, 90];
+const student = {name: 'Sabbir Sarker', job: 'khawadawa kora'}
+// console.log(Array.isArray( numbers));
+console.log(Array.isArray(student));
+console.log(typeof student);
